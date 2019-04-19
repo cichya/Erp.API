57cd9d9f0dc334cce00132189820e27c9babc01d
Adrian <adrcichy@outlook.com>
2019-04-19 22:31:04 +0200

    Employee model added

---
+++ Erp.API/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Erp.API.Models
+{
+	public class EmployeeModel
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime Birth { get; set; }
+		public string WorkingPosition { get; set; }
+		public decimal Salary { get; set; }
+		public int TaxNumber { get; set; }
+	}
+}
