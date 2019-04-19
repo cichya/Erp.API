380dad7539b9dc1b7468c4b78755368735f317b4
Adrian <adrcichy@outlook.com>
2019-04-19 23:01:39 +0200

    automapper added, employee to employee for list DTO transform

---
+++ Erp.API/Helpers/AutoMapperHelper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Erp.API.DTOs;
+using Erp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Erp.API.Helpers
+{
+	public class AutoMapperHelper : Profile
+	{
+		public AutoMapperHelper()
+		{
+			this.CreateMap<EmployeeModel, EmployeeForListDto>()
+				.ForMember(member => member.Age, memberOpt =>
+				{
+					memberOpt.ResolveUsing(value => value.Birth.CalculateAge());
+				});
+		}
+	}
+}
