a14aaa6a7a02754bf8660daf5da81374f77d56fc
Adrian <adrcichy@outlook.com>
2019-04-19 22:52:36 +0200

    calculate age ext method added with tests

---
+++ Erp.API.Tests/ExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using Erp.API.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Erp.API.Tests
+{
+	[TestClass]
+	public class ExtensionsTests
+	{
+		[TestMethod]
+		public void CalculateAge_Equal_3_Years_Return_2_Test()
+		{
+			DateTime birth = DateTime.Now.AddYears(-3);
+
+			int result = birth.CalculateAge();
+
+			Assert.AreEqual(2, result);
+		}
+
+		[TestMethod]
+		public void CalculateAge_More_Than_3_Years_Return_3_Test()
+		{
+			DateTime birth = DateTime.Now.AddYears(-3).AddDays(-1);
+
+			int result = birth.CalculateAge();
+
+			Assert.AreEqual(3, result);
+		}
+
+		private int CalculateAge(DateTime birth)
+		{
+			var age = DateTime.Today.Year - birth.Year;
+
+			if (birth.AddYears(age) > DateTime.Today)
+			{
+				age--;
+			}
+
+			return age;
+		}
+	}
+}
---
+++ Erp.API/Helpers/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Erp.API.Helpers
+{
+	public static class Extensions
+	{
+		public static int CalculateAge(this DateTime birth)
+		{
+			var age = DateTime.Today.Year - birth.Year;
+
+			if (birth.AddYears(age) > DateTime.Today)
+			{
+				age--;
+			}
+
+			return age;
+		}
+	}
+}
