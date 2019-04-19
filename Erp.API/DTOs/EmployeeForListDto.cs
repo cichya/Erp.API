8f096023501795b7342195541e78113eac26f61d
Adrian <adrcichy@outlook.com>
2019-04-19 22:55:56 +0200

    Employee for list DTO added

---
+++ Erp.API/DTOs/EmployeeForListDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Erp.API.DTOs
+{
+	public class EmployeeForListDto
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+		public string WorkingPosition { get; set; }
+		public decimal Salary { get; set; }
+		public int TaxNumber { get; set; }
+	}
+}
