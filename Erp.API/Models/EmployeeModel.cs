a4b8ba02b78e2f6133d0f353f4ed8d8491792f8e
Adrian <adrcichy@outlook.com>
2019-04-25 21:17:13 +0200

    added xml attributes to model

--- Erp.API/Models/EmployeeModel.cs
+++ Erp.API/Models/EmployeeModel.cs
@@ -1,15 +1,30 @@
 ﻿using System;
+using System.Xml.Serialization;
 
 namespace Erp.API.Models
 {
+	[XmlType("employee")]
 	public class EmployeeModel
 	{
+		[XmlElement(ElementName = "id")]
 		public int Id { get; set; }
+
+		[XmlElement(ElementName = "firstName")]
 		public string FirstName { get; set; }
+
+		[XmlElement(ElementName = "lastName")]
 		public string LastName { get; set; }
+
+		[XmlElement(ElementName = "birth")]
 		public DateTime Birth { get; set; }
+
+		[XmlElement(ElementName = "workingPosition")]
 		public string WorkingPosition { get; set; }
+
+		[XmlElement(ElementName = "salary")]
 		public decimal Salary { get; set; }
+
+		[XmlElement(ElementName = "taxNumber")]
 		public int TaxNumber { get; set; }
 	}
 }
